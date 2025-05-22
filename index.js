24d05a809ccea0a66074e9f1a8d83192064e1993
Subiksha-it <subiit111@gmail.com>
2025-05-22 16:00:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+import cors from "cors"
+import connectDB from "./database/connectDB.js"
+import userRoutes from "./routes/userRoutes.js"
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+
+app.use("/api/user",userRoutes)
+
+app.get("/",(req,res) =>{
+    const {name} = req.body;
+    res.status(200).json({Message:"Api change successfully"})
+})
+
+app.get("/api/heart-beat",(req,res) =>{
+    res.status(200).json({data:"Heart beat was successful"})
+}
+)
+
+app.listen(process.env.port, ()=>{
+    console.log(`Server running successfully at port ${process.env.PORT}`)
+}
+)
+
+connectDB();
